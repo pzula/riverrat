ca48e2134fec8a5125e44a5fae09dd88f7c22409
Persa Zula <persa@persazula.com>
2014-02-20 16:36:01 -0700

    Adds User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe User do
+  describe "with valid params" do
+    it "creates a user" do
+      FactoryGirl.build(:user).should be_valid
+    end
+  end
+
+  describe "with invalid params" do
+    it "does not create a user" do
+      FactoryGirl.build(:user, :email => nil).should_not be_valid
+    end
+  end
 end
