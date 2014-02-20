35fc5efd1c05345e2fc322b2dc47def185bbec6d
Brian Winterling <bwinterling@yahoo.com>
2014-02-20 16:20:43 -0700

    first steps to moving over map.  currently incomplete

---
+++ app/assets/javascripts/map/map.js
@@ -0,0 +1,23 @@
+$(function() {
+  "use strict";
+  Map.defaultLat = 39.5500;
+  Map.defaultLong = -107.3167;
+  Map.defaultZoom = 9;
+
+  Map.plotGauges = function(geojson) {
+    L.mapbox.map('map', 'srtsrt32.haldb884')
+      .setView([Map.defaultLat, Map.defaultLong], Map.defaultZoom)
+      .featureLayer.setGeoJSON(geojson);
+  };
+
+  Map.fetchGauges = function(callback) {
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: 'http://localhost:5000/api/v1/gauges',
+      success: callback
+    });
+  };
+
+  Map.fetchGauges(Map.plotGauges);
+});
---
+++ app/controllers/api/v1/gauges_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::GaugesController < ApplicationController
+
+  def index
+    # gauges = ??
+    # render json: gauges
+  end
+
+end
