da27cd88b524b01f3582a01727c8b812d119ec4e
Persa Zula <persa@persazula.com>
2014-02-20 16:52:02 -0700

    Updates the create account submission button

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -8,7 +8,7 @@ describe "A visitor" do
     fill_in "Email",  with:  "example@example.com"
     fill_in "Password", with: "123456"
     fill_in "Password confirmation", with: "123456"
-    click_on "Create my username!"
+    click_on "Create my account!"
     expect(page).to have_text("Thank you for signing up!")
   end
 end
