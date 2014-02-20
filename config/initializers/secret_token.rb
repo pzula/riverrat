15bfe0103e33d238a3dcd31542a4fbd6d12247e7
Persa Zula <persa@persazula.com>
2014-02-20 11:23:36 -0700

    Generating basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Riverrat::Application.config.secret_key_base = 'c614457aefdbe4e32e21fdc35b09fe43dd368ab44c4c7f53b672ca324d85b9ac7ec9d913ae6df98ec4160092c5e91b6bce81fd5f7457b7ddc360d2781fcc46f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Riverrat::Application.config.session_store :cookie_store, key: '_riverrat_session'
