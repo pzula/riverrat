17d020915484e735daf5c6ce7e49d5e590232e1a
Persa Zula <persa@persazula.com>
2014-02-20 16:28:22 -0700

    Adds Users controller and creation form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,4 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_uniqueness_of :email
 
-private
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
-
 end
--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -4,6 +4,11 @@ describe "A visitor" do
   it "can sign up" do
     visit "/"
     click_on "Sign Up"
-    expect(page).to have_content("Choose a username")
+    expect(page).to have_field("Email")
+    fill_in "Email",  with:  "example@example.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_on "Create my username!"
+    expect(page).to have_text("Thank you for signing up!")
   end
 end
