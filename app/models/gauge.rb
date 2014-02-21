0439450997a453c158b1f739556c120158886608
Simon Taranto <simon.taranto@gmail.com>
2014-02-20 22:13:31 -0700

    move guages api url into own method

--- app/models/gauge.rb
+++ app/models/gauge.rb
@@ -1,9 +1,14 @@
 class Gauge
 
   def self.all_gauges_in_geojson
-    url = Rails.configuration.api_base_url + '/api/v1/gauges'
-    response = Faraday.get url
+    response = Faraday.get api_gauges_url
     json = JSON.parse(response.body)
+  end
+
+  private
+
+  def self.api_gauges_url
+    Rails.configuration.api_base_url + '/api/v1/gauges'
   end
 
 end
