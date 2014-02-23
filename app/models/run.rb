05b5c5527f730ecb0cc412f6c382e66f333de5a3
Simon Taranto <simon.taranto@gmail.com>
2014-02-23 16:38:22 -0700

    complete run model and association to river

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,7 @@
 class Run < ActiveResource::Base
 
+  self.site = "http://localhost:5555/api/v1/"
+
+  self.prefix = "/api/v1/rivers/:river_id/"
 
 end
--- spec/models/river_spec.rb
+++ spec/models/river_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe River do
   before :each do
-    @river = {:river => {:name => "Black River"}}.to_json
+    @river = {:river => {:name => "Black River",
+                         :id => 1}}.to_json
+    @runs = {:runs => [
+      {:name => "Soot",
+       :river_id => 1},
+      {:name => "Ash",
+       :river_id => 1}
+    ]
+    }.to_json
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get "/api/v1/rivers/1.json", {}, @river
+      mock.get "/api/v1/rivers/1/runs.json", {}, @runs
     end
   end
 
