f38af81506b15ac341792cf4edbc3f13b405b81b
Simon Taranto <simon.taranto@gmail.com>
2014-02-25 10:15:11 -0700

    flesh out favorites#create method and write test for user.favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,7 @@
 class FavoritesController < ApplicationController
 
   def create
+    current_user.favorites.create(:river_id => params[:river_id])
     redirect_to :back
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  describe "with valid params" do
-    it "creates a user" do
-      FactoryGirl.build(:user).should be_valid
+
+  describe "creation" do
+    describe "with valid params" do
+      it "creates a user" do
+        FactoryGirl.build(:user).should be_valid
+      end
+    end
+
+    describe "with invalid params" do
+      it "does not create a user" do
+        FactoryGirl.build(:user, :email => nil).should_not be_valid
+      end
     end
   end
 
-  describe "with invalid params" do
-    it "does not create a user" do
-      FactoryGirl.build(:user, :email => nil).should_not be_valid
-    end
+  describe "favorites" do
+    user = FactoryGirl.create(:user, :email => "foo@example.com")
+    FactoryGirl.create(:favorite, :user_id => user.id, :river_id => 1)
+    (user.favorites.first).should be_kind_of(Favorite)
   end
+
 end
