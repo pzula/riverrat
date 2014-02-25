7e9750dabfe254ac55a2339cfd765ce5b64147ae
Simon Taranto <simon.taranto@gmail.com>
2014-02-25 10:26:38 -0700

    create Favorite model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220214416) do
+ActiveRecord::Schema.define(version: 20140225172541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "river_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "favorites", ["river_id"], name: "index_favorites_on_river_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
