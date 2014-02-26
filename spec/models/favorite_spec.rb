6c837f7e4498a98ff2fc90a7c9b5f9f31c4445f8
Simon Taranto <simon.taranto@gmail.com>
2014-02-26 09:45:44 -0700

    add uniqueness validation on river favorites

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Favorite do
-  xit "does not create duplicate favorites" do
+  it "does not create duplicate favorites" do
+    FactoryGirl.create(:favorite, :user_id => 1, :river_id => 1).should be_valid
+    FactoryGirl.build(:favorite, :user_id => 1, :river_id => 1).should_not be_valid
   end
 end
