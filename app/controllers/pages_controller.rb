666b10de8a6ac9c1035ba652ec3ab51bcf761831
Simon Taranto <simon.taranto@gmail.com>
2014-02-26 21:55:52 -0700

    add prev button to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,18 @@
 class PagesController < ApplicationController
   def home
-    offset = params[:offset] || 0
-    @next_offset = offset.to_i + 10
-    @rivers = River.find(:all, params: {:offset => offset, :limit => 10})
+    current_offset = params[:offset] || 0
+    set_offsets(current_offset)
+    @rivers = River.find(:all, params: {:offset => current_offset, :limit => 10})
     @gauges = Gauge.all.to_json
     @favorite_rivers = []
     if current_user
       favorite_river_ids = current_user.favorites.map(&:river_id)
       @favorite_rivers = @rivers.select { |river| favorite_river_ids.include?(river.id) }
     end
+  end
+
+  def set_offsets(offset)
+    @prev_offset = offset.to_i == 0 ? 0 : offset.to_i - 10
+    @next_offset = offset.to_i + 10
   end
 end
